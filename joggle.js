290e3651a4a3e196b78ce85c372cb05031d7b7c2
Aecobalenorf <2805261212@qq.com>
2023-04-01 11:53:23 +0800

    Initial commit

---
+++ joggle.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 80;
+
+// 添加一个 GET 请求的路由
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// 启动服务
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
